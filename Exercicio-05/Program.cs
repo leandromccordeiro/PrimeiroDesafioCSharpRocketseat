ccbd7742ec310666b6a280ef5483a67177cf8939
Leandro Cordeiro <leandro.marcos@supermercadosbh.com.br>
2024-04-18 13:36:20 -0300

    exercicio 5 ready

--- Exercicio-05/Program.cs
+++ Exercicio-05/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercicio_05;
+
+Console.WriteLine("Informe a placa do vículo:");
+Veiculo veiculo = new Veiculo(Console.ReadLine());
+
+bool verificador = veiculo.ValidaPlaca();
+
+Console.WriteLine(verificador);
---
+++ Exercicio-05/Veiculo.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Exercicio_05
+{
+    public class Veiculo
+    {
+        public string Placa { get; set; }
+
+        public Veiculo(string placa)
+        {
+            Placa = placa;
+        }
+
+        public bool ValidaPlaca()
+        {
+            if (Placa.Trim().Length != 7)
+            {
+                return false;
+                //Console.WriteLine("A placa informada possui quantidade de caracteres diferente do padrão '7'.");
+            }
+            else if (int.TryParse(Placa.Substring(0, 1), out int result) || int.TryParse(Placa.Substring(1, 1), out int result1)
+                || int.TryParse(Placa.Substring(2, 1), out int result2))
+            {
+                return false;
+                //Console.WriteLine("Os 3 primeiros caracteres precisam ser representados somente por textos.");
+            }
+            else if (!int.TryParse(Placa.Substring(3), out int caractNum))
+            {
+                return false;
+                //Console.WriteLine("Os 4 últimos caracteres precisam ser representados somente por numeros.");
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
